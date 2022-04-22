43d4bb838fddcf0985f285ef843d3c43570c4916
JanLuczewski <jan.luczewski@gmail.com>
2022-04-22 15:18:38 +0200

    pierwszy coomit

---
+++ scheduler.py
@@ -0,0 +1,28 @@
+from openpyxl import load_workbook
+from pathlib import Path
+from docxtpl import DocxTemplate
+
+base_dir = Path(__file__).parent
+workbook = load_workbook(filename=base_dir / "odbiory.xlsx")
+sheet = workbook.active
+
+word_template_path = base_dir / "formatka_A35.docx"
+doc = DocxTemplate(word_template_path)
+output_dir = base_dir / "OUTPUT"
+output_dir.mkdir(exist_ok=True)
+
+for row in sheet.iter_rows(min_row=2,values_only=True):
+    Inspection_ID = row[1]
+    Day = row[3]
+    Starting_time = row[4]
+    Description = row[15]
+    context = {
+        "Inspection_ID" : Inspection_ID,
+        "Description" : Description,
+        "Starting_time" : Starting_time,
+        "Day" : Day,
+    }
+    doc.render(context)
+    doc.save(base_dir / f"{row['Inspection_ID']}_wypluty.docx")
+
+
