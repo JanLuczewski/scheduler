2eaa4a2cebbf6bc5294a7316df4ac365b3fe2104
JanLuczewski <jan.luczewski@gmail.com>
2022-04-25 10:50:27 +0200

    teraz wypluwa tylko moje

--- scheduler.py
+++ scheduler.py
@@ -3,9 +3,6 @@ from pathlib import Path
 from docxtpl import DocxTemplate
 import argparse
 """ scheduler.py służy do tworzenia raportów dla inspekcji.
-Potrzebne są dwa argumenty :
-- source   czyli źródłowy plik excel z odbiorami zgłoszonymi do Calypso
-- template   czyli plik służący za formatkę dla danego projektu stoczniowego
 Program wywołuje się wpisując w terminalu 
     python3 scheduler.py source template
 przykładowo
@@ -14,12 +11,14 @@ wygenerowane raporty można znaleźć w folderze OUTPUT
 """
 
 parser = argparse.ArgumentParser(description='wypluwa raporty dla odbiorow')
-parser.add_argument('source', metavar='zrodlo', type=str, help='wpisz nazwe pliku zrodlowego')
-parser.add_argument('template', metavar='formatka', type=str, help='wpisz nazwe pliku z formatka')
+parser.add_argument('source', metavar='zrodlo', type=str, help='source   czyli źródłowy plik excel z odbiorami zgłoszonymi do Calypso')
+parser.add_argument('template', metavar='formatka', type=str, help='template   czyli plik służący za formatkę dla danego projektu stoczniowego')
 args = parser.parse_args()
 
 source = args.source
 template = args.template
+
+
 
 base_dir = Path(__file__).parent
 workbook = load_workbook(filename=base_dir / source)
@@ -37,13 +36,16 @@ for row in sheet.iter_rows(min_row=2,values_only=True):
     Starting_time = row[4]
     Description = row[15]
     Inspector = row[10]
-    context = {
-        "Inspection_ID" : Inspection_ID,
-        "Description" : Description,
-        "Starting_time" : Starting_time,
-        "Day" : Day,
-    }
-    doc.render(context)
-    doc.save(output_dir / f"{Inspection_ID}.docx")
+    JanLuczewski = "JAN LUCZEWSKI"
+    if Inspector == JanLuczewski:
+        context = {
+            "Inspection_ID" : Inspection_ID,
+            "Description" : Description,
+            "Starting_time" : Starting_time,
+            "Day" : Day,
+        }
+        doc.render(context)
+        doc.save(output_dir / f"{Inspection_ID}.docx")
+
 
 
